c67c74912fe3d75b512900b362a080e1ae148099
Jason Fowler <jason@thebrazengeek.com>
2019-07-01 06:45:09 +0800

    Continued linting/vetting

--- pb.go
+++ pb.go
@@ -73,6 +73,21 @@ var (
 	isCygwinTerminal = isatty.IsCygwinTerminal
 )
 
+// Presets
+var (
+	// Full - preset with all default available elements
+	// Example: 'Prefix 20/100 [-->______] 20% 1 p/s ETA 1m Suffix'
+	Full ProgressBarTemplate = `{{string . "prefix"}}{{counters . }} {{bar . }} {{percent . }} {{speed . }} {{rtime . "ETA %s"}}{{string . "suffix"}}`
+
+	// Default - preset like Full but without elapsed time
+	// Example: 'Prefix 20/100 [-->______] 20% 1 p/s ETA 1m Suffix'
+	Default ProgressBarTemplate = `{{string . "prefix"}}{{counters . }} {{bar . }} {{percent . }} {{speed . }}{{string . "suffix"}}`
+
+	// Simple - preset without speed and any timers. Only counters, bar and percents
+	// Example: 'Prefix 20/100 [-->______] 20% Suffix'
+	Simple ProgressBarTemplate = `{{string . "prefix"}}{{counters . }} {{bar . }} {{percent . }}{{string . "suffix"}}`
+)
+
 // ProgressBar is the main object of bar
 type ProgressBar struct {
 	current, total int64
@@ -137,7 +152,6 @@ func (pb *ProgressBar) configure() {
 
 		pb.defaultVars()
 		pb.defaultOutput()
-
 
 		if pb.tmpl == nil {
 			pb.tmpl, pb.err = getTemplate(string(Default))
